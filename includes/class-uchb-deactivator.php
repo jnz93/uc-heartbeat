7b763097357e4d78c763ec19d1d77625457c01a6
Joanez Andrades <suportedeathofday@gmail.com>
2020-10-24 14:20:17 -0300

    First Commit

---
+++ includes/class-uchb-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       unitycode.tech
+ * @since      1.0.0
+ *
+ * @package    Uchb
+ * @subpackage Uchb/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Uchb
+ * @subpackage Uchb/includes
+ * @author     jnz93 <contato@unitycode.tech>
+ */
+class Uchb_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
