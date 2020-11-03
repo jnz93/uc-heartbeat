a4dedb63a127b70ea7c65c0533142e82f0c00554
Joanez Andrades <suportedeathofday@gmail.com>
2020-11-03 16:50:22 -0300

    Templates: cadastrar projeto e cadastrar clientes

--- admin/class-uchb-admin.php
+++ admin/class-uchb-admin.php
@@ -130,6 +130,195 @@ class Uchb_Admin {
 	 */
 	public function construct_admin_page()
 	{
-		echo 'Construa a página aqui';
+		$buttons = '<p uk-margin>
+						<button class="uk-button uk-button-default uk-button-large" uk-toggle="target: #modal-projetos">Projetos</button>
+						<button class="uk-button uk-button-default uk-button-large" uk-toggle="target: #modal-orcamentos">Orçamentos</button>
+						<button class="uk-button uk-button-default uk-button-large" uk-toggle="target: #modal-clientes">Clientes</button>
+						<button class="uk-button uk-button-default uk-button-large" uk-toggle="target: #modal-financas">Finanças</button>
+					</p>';
+
+		$card_buttons = '<div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
+							<div>
+								<div class="uk-card uk-card-default uk-card-body">
+									<h3 class="uk-card-title">Menu Principal</h3>
+									'. $buttons .'
+								</div>
+							</div>
+						</div>';
+
+	
+		// Modal cadastro projeto
+		$modal_projetos = '<div id="modal-projetos" class="uk-modal-container" uk-modal>
+								<div class="uk-modal-dialog uk-modal-body">
+									<button class="uk-modal-close-default" type="button" uk-close></button>
+									<div class="uk-modal-header">
+										<h2 class="uk-modal-title">Cadastrar Projeto</h2>
+									</div>
+									<div class="uk-modal-body">
+										<form class="uk-grid-small" uk-grid>
+											<div class="uk-width-1-2">
+												<label class="uk-form-label" for="uchb_project_name">Nome do projeto</label>
+												<div class="uk-form-controls">
+													<input class="uk-input" id="uchb_project_name" type="text" placeholder="Adicione um nome para o projeto">
+												</div>
+											</div>
+											<div class="uk-width-1-2">
+												<label class="uk-form-label" for="uchb_project_description">Descrição do projeto</label>
+												<div class="uk-form-controls">
+													<input class="uk-input" id="uchb_project_description" type="text" placeholder="Adicione uma descrição rápida">
+												</div>
+											</div>
+											<div class="uk-width-1-2">
+												<label class="uk-form-label" for="uchb_project_specs">Especificações</label>
+												<div class="uk-form-controls">
+													<textarea class="uk-input" id="uchb_project_specs"></textarea>
+												</div>
+											</div>
+											<div class="uk-width-1-2">
+												<label class="uk-form-label" for="uchb_project_details">Detalhes</label>
+												<div class="uk-form-controls">
+													<textarea class="uk-input" id="uchb_project_details"></textarea>
+												</div>
+											</div>
+											<div class="uk-width-1-2">
+												<label class="uk-form-label" for="uchb_project_types">Tipo do projeto</label>
+												<div class="uk-form-controls">
+													<select class="uk-select" id="uchb_project_types">
+														<option>Option 01</option>
+														<option>Option 02</option>
+													</select>
+												</div>
+											</div>
+											<div class="uk-width-1-2">
+												<label class="uk-form-label" for="uchb_project_customer">Cliente</label>
+												<div class="uk-form-controls">
+													<select class="uk-select" id="uchb_project_customer">
+														<option>Option 01</option>
+														<option>Option 02</option>
+													</select>
+												</div>
+											</div>
+											<div class="uk-width-1-1">
+												<label class="uk-form-label" for="uchb_project_proposal">Proposta Unitycode</label>
+												<div class="uk-form-controls">
+													<textarea class="uk-input" id="uchb_project_proposal"></textarea>
+												</div>
+											</div>
+											<div class="uk-width-1-4">
+												<label class="uk-form-label" for="uchb_project_deadline">Previsão de entrega</label>
+												<div class="uk-form-controls">
+													<input class="uk-input" id="uchb_project_deadline" type="date">
+												</div>
+											</div>
+											<div class="uk-width-1-4">
+												<label class="uk-form-label" for="uchb_project_hours">Orçamento em Horas</label>
+												<div class="uk-form-controls">
+													<input class="uk-input" id="uchb_project_hours" type="number" placeholder="Ex: 10">
+												</div>
+											</div>
+											<div class="uk-width-1-4">
+												<label class="uk-form-label" for="uchb_project_price">Valor do projeto</label>
+												<div class="uk-form-controls">
+													<input class="uk-input" id="uchb_project_price" type="number" placeholder="Ex: 1800,00">
+												</div>
+											</div>
+											<div class="uk-width-1-4">
+												<label class="uk-form-label" for="uchb_project_status">Status</label>
+												<div class="uk-form-controls">
+													<select class="uk-select" id="uchb_project_status">
+														<option>Proposta enviada</option>
+														<option>Em Andamento</option>
+														<option>Parado</option>
+														<option>Cancelado</option>
+														<option>Concluído</option>
+													</select>
+												</div>
+											</div>
+										</form>
+									</div>
+									<div class="uk-modal-footer uk-text-right">
+										<button class="uk-button uk-button-default uk-modal-close" type="button">Cancelar</button>
+										<button class="uk-button uk-button-primary" type="button">Salvar Projeto</button>
+									</div>
+								</div>
+							</div>'; 
+
+		// Modal cadastrar clientes
+		$modal_cliente = '<div id="modal-clientes" class="uk-modal-container" uk-modal>
+							<div class="uk-modal-dialog uk-modal-body">
+								<button class="uk-modal-close-default" type="button" uk-close></button>
+								<div class="uk-modal-header">
+									<h2 class="uk-modal-title">Cadastrar Cliente</h2>
+								</div>
+								<div class="uk-modal-body">
+									<form class="uk-grid-small" uk-grid>
+										<div class="uk-width-1-2">
+											<label class="uk-form-label" for="uchb_client_name">Nome do Cliente</label>
+											<div class="uk-form-controls">
+												<input class="uk-input" id="uchb_client_name" type="text" placeholder="Adicione o nome do cliente">
+											</div>
+										</div>
+										<div class="uk-width-1-2">
+											<label class="uk-form-label" for="uchb_client_company">Nome da empresa</label>
+											<div class="uk-form-controls">
+												<input class="uk-input" id="uchb_client_company" type="text" placeholder="Adicione o nome da empresa">
+											</div>
+										</div>
+										<div class="uk-width-1-2">
+											<label class="uk-form-label" for="uchb_client_doc">CPF/CNPJ</label>
+											<div class="uk-form-controls">
+												<input class="uk-input" id="uchb_client_doc" type="text" placeholder="Adicione o CPF ou CNPJ">
+											</div>
+										</div>
+										<div class="uk-width-1-2">
+											<label class="uk-form-label" for="uchb_client_email">E-mail</label>
+											<div class="uk-form-controls">
+												<input class="uk-input" id="uchb_client_email" type="email" placeholder="E-mail para contato e notificações">
+											</div>
+										</div>
+										<div class="uk-width-1-2">
+											<label class="uk-form-label" for="uchb_client_telnumber">WhatsApp/Telefone</label>
+											<div class="uk-form-controls">
+												<input class="uk-input" id="uchb_client_telnumber" type="text" placeholder="Whatsapp/Telefone para contato e notificações">
+											</div>
+										</div>
+										<div class="uk-width-1-2">
+											<label class="uk-form-label" for="uchb_client_type">Tipo Cliente</label>
+											<div class="uk-form-controls">
+												<select class="uk-select" id="uchb_project_type">
+													<option>Option 01</option>
+													<option>Option 02</option>
+													<option>Option 03</option>
+													<option>Option 04</option>
+													<option>Option 05</option>
+												</select>
+											</div>
+										</div>
+										<div class="uk-width-1-2">
+											<label class="uk-form-label" for="uchb_client_branch">Ramo da Empresa</label>
+											<div class="uk-form-controls">
+												<input class="uk-input" id="uchb_client_branch" type="text" placeholder="Ex: Telecomunicações">
+											</div>
+										</div>
+										<div class="uk-width-1-2">
+											<label class="uk-form-label" for="uchb_client_address">Endereço</label>
+											<div class="uk-form-controls">
+												<input class="uk-input" id="uchb_client_address" type="text" placeholder="Ex: Av. tal, 0001 - Cidade - UF">
+											</div>
+										</div>
+									</form>
+								</div>
+								<div class="uk-modal-footer uk-text-right">
+									<button class="uk-button uk-button-default uk-modal-close" type="button">Cancelar</button>
+									<button class="uk-button uk-button-primary" type="button">Salvar Projeto</button>
+								</div>
+							</div>
+						</div>';
+
+		
+		// Outputs
+		echo $card_buttons;
+		echo $modal_projetos;
+		echo $modal_cliente;
 	}
 }
